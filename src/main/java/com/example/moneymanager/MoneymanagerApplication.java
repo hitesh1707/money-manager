9793a89470551b2f59d72a9d722b3746d95cc552
Hitesh Kumar <hk0004509@gmail.com>
2026-03-03 13:10:47 +0530

    commit

--- src/main/java/com/example/moneymanager/MoneymanagerApplication.java
+++ src/main/java/com/example/moneymanager/MoneymanagerApplication.java
@@ -1,14 +1,15 @@
 package com.example.moneymanager;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mail.MailSenderAutoConfiguration;
+import org.springframework.boot.SpringApplication;
 
-@SpringBootApplication
+@SpringBootApplication(
+		exclude = { MailSenderAutoConfiguration.class }
+)
 public class MoneymanagerApplication {
 
 	public static void main(String[] args) {
-
 		SpringApplication.run(MoneymanagerApplication.class, args);
 	}
-
 }
