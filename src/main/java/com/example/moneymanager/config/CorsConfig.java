91c4f9d5086294d384688fdc09f1656c151c8af2
Hitesh Kumar <hk0004509@gmail.com>
2026-03-08 15:06:52 +0530

    initial commit

--- src/main/java/com/example/moneymanager/config/CorsConfig.java
+++ src/main/java/com/example/moneymanager/config/CorsConfig.java
@@ -14,13 +14,11 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins(
-                                "http://localhost:5174",
-                                "https://money-manager-react-fawn.vercel.app"
-                        )
+                        .allowedOriginPatterns("*")
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
+
             }
         };
     }
