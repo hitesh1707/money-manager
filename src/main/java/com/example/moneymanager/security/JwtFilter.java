91217af108cbb998ba932666cda2cfd93ad9ac06
Hitesh Kumar <hk0004509@gmail.com>
2026-03-08 15:34:51 +0530

    initial commit

--- src/main/java/com/example/moneymanager/security/JwtFilter.java
+++ src/main/java/com/example/moneymanager/security/JwtFilter.java
@@ -26,6 +26,10 @@ public class JwtFilter extends OncePerRequestFilter {
                                     HttpServletResponse response,
                                     FilterChain filterChain)
             throws ServletException, IOException {
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            filterChain.doFilter(request, response);
+            return;
+        }
 
         String authHeader = request.getHeader("Authorization");
 
